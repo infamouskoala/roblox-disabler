018b5a42a688a64e665cb9f62a6033aee8d93bc4
Koala <75808182+infamouskoala@users.noreply.github.com>
2024-05-15 21:37:24 +0530

    Add files via upload

---
+++ src/notsrc.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(){
+
+    char *nobypasslmfao;
+
+    system("title x64dbg");
+    printf("Killing roblox now >w<, done with love by infamouskoala and humancarpet18 <3");
+    gets(nobypasslmfao);
+}
